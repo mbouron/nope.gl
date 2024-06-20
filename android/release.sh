d0ce5e319230e0bbd0399a33d8a534755a2c0c63
satyan <satyan@mojo.video>
2024-06-20 16:44:07 +0200

    Add github action to publish snapshots and releases

---
+++ android/release.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -xeu
+
+self=$(readlink -f "$0")
+self_dir=$(dirname "$self")
+base_dir=$(dirname "$self_dir")
+
+cd "$base_dir"
+
+if [ ! -f "configure.py" ]; then
+    echo "$0 must be executed from the project root directory"
+    exit 1
+fi
+
+archs="arm aarch64 x86_64"
+for arch in $archs; do
+    python configure.py --buildtype "debug" --build-id --host Android --host-arch "$arch"
+    make -f "Makefile.Android.$arch"
+done
+
+export NGL_ANDROID_ENV="$base_dir/Android/"
+(
+    cd android
+    ./gradlew publish
+)
