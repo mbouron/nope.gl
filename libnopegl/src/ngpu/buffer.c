05159a66b89bd91b705be607a487d069e9ae15b1
Matthieu Bouron <matthieu.bouron@gmail.com>
2025-12-20 14:04:33 +0100

    ngpu: add NGPU_BUFFER_WHOLE_SIZE
    NGPU_BUFFER_WHOLE_SIZE can be passed to ngpu_buffer_map() and
    ngpu_buffer_upload() to use the whole size of the buffer.

--- libnopegl/src/ngpu/buffer.c
+++ libnopegl/src/ngpu/buffer.c
@@ -62,6 +62,10 @@ int ngpu_buffer_upload(struct ngpu_buffer *s, const void *data, size_t offset, s
     int ret = ngpu_buffer_wait(s);
     if (ret < 0)
         return ret;
+
+    if (size == NGPU_BUFFER_WHOLE_SIZE)
+        size = s->size;
+
     return s->gpu_ctx->cls->buffer_upload(s, data, offset, size);
 }
 
@@ -70,6 +74,10 @@ int ngpu_buffer_map(struct ngpu_buffer *s, size_t offset, size_t size, void **da
     int ret = ngpu_buffer_wait(s);
     if (ret < 0)
         return ret;
+
+    if (size == NGPU_BUFFER_WHOLE_SIZE)
+        size = s->size;
+
     return s->gpu_ctx->cls->buffer_map(s, offset, size, datap);
 }
 
