5faeed42764d5f0e2b7e9557fad1397dee981ba8
Matthieu Bouron <matthieu.bouron@gmail.com>
2026-08-21 16:48:41 +0200

    paint,drawrect2d: store direct uniform data pointers

--- libnopegl/src/node_paint.h
+++ libnopegl/src/node_paint.h
@@ -23,6 +23,7 @@
 #define NODE_PAINT_H
 
 #include <stddef.h>
+#include <stdint.h>
 #include <ngpu/ngpu.h>
 #include "utils/darray.h"
 
@@ -45,7 +46,7 @@ struct bstr;
 struct paint_uniform_def {
     char name[PAINT_NAME_LEN];
     enum ngpu_type type;
-    size_t opts_offset;   /* byte offset into paint node opts struct */
+    const uint8_t *data;
 };
 
 struct paint_custom_uniform_def {
