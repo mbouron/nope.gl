25370f7926a342281bfea78014e4289878a172e0
Matthieu Bouron <matthieu.bouron@gmail.com>
2026-08-30 14:30:22 +0200

    nodes: centralize children traversal
    scene.c and nodes.c had separate children traversal logics, and the public path
    missed NODEDICT parameters. Move children_apply_func() to nodes.c, rename it to
    ngli_node_children_apply() and build ngl_node_get_children() on top of it.

--- libnopegl/src/scene.c
+++ libnopegl/src/scene.c
@@ -32,59 +32,6 @@
 
 NGLI_RC_CHECK_STRUCT(ngl_scene);
 
-typedef int (*children_func_type)(void *user_arg, struct ngl_node *parent, struct ngl_node *node);
-
-/*
- * Apply a function on all children by walking through them. This is useful when
- * node->children is not yet initialized (or confirmed to be complete yet).
- */
-static int children_apply_func(children_func_type func, void *user_arg, struct ngl_node *node)
-{
-    uint8_t *base_ptr = node->opts;
-    const struct node_param *par = node->cls->params;
-
-    if (!par)
-        return 0;
-
-    while (par->key) {
-        uint8_t *parp = base_ptr + par->offset;
-
-        if (par->type == NGLI_PARAM_TYPE_NODE || (par->flags & NGLI_PARAM_FLAG_ALLOW_NODE)) {
-            struct ngl_node *child = *(struct ngl_node **)parp;
-            if (child) {
-                int ret = func(user_arg, node, child);
-                if (ret < 0)
-                    return ret;
-            }
-        } else if (par->type == NGLI_PARAM_TYPE_NODELIST) {
-            uint8_t *elems_p = parp;
-            uint8_t *nb_elems_p = parp + sizeof(struct ngl_node **);
-            struct ngl_node **elems = *(struct ngl_node ***)elems_p;
-            const size_t nb_elems = *(size_t *)nb_elems_p;
-            for (size_t i = 0; i < nb_elems; i++) {
-                struct ngl_node *child = elems[i];
-                int ret = func(user_arg, node, child);
-                if (ret < 0)
-                    return ret;
-            }
-        } else if (par->type == NGLI_PARAM_TYPE_NODEDICT) {
-            struct hmap *hmap = *(struct hmap **)parp;
-            if (hmap) {
-                const struct hmap_entry *entry = NULL;
-                while ((entry = ngli_hmap_next(hmap, entry))) {
-                    struct ngl_node *child = entry->data;
-                    int ret = func(user_arg, node, child);
-                    if (ret < 0)
-                        return ret;
-                }
-            }
-        }
-        par++;
-    }
-
-    return 0;
-}
-
 static int reset_nodes(void *user_arg, struct ngl_node *parent, struct ngl_node *node)
 {
     struct ngl_scene *s = user_arg;
@@ -102,7 +49,7 @@ static int reset_nodes(void *user_arg, struct ngl_node *parent, struct ngl_node 
 
     ngli_assert(!node->ctx);
 
-    int ret = children_apply_func(reset_nodes, s, node);
+    int ret = ngli_node_children_apply(reset_nodes, s, node);
     ngli_assert(ret == 0);
 
     ngli_darray_reset(&node->children);
@@ -248,7 +195,7 @@ static int add_scene_edge_at(struct ngl_scene *s, struct ngl_node *parent,
     add_runtime_edge(parent, child, index);
     add_scene_node(s, child);
 
-    const int ret = children_apply_func(add_scene_edge, s, child);
+    const int ret = ngli_node_children_apply(add_scene_edge, s, child);
     if (ret < 0) {
         remove_scene_children(child);
         remove_scene_node(s, child);
@@ -348,7 +295,7 @@ static int attach_root(struct ngl_scene *s, struct ngl_node *node)
 
     add_scene_node(s, s->params.root);
 
-    int ret = children_apply_func(add_scene_edge, s, s->params.root);
+    int ret = ngli_node_children_apply(add_scene_edge, s, s->params.root);
     if (ret < 0)
         goto fail;
 
